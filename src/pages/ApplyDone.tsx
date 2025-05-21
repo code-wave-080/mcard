e13d78d92b60e87631bba302a370fd94a1975afb
code-wave <codewave080@gmail.com>
2025-05-21 23:23:06 +0900

    refactor: ApplyDone 페이지를 styled-components를 사용하여 리팩토링
    가독성과 유지보수성 향상을 위해 기존 레이아웃 컴포넌트를 styled-components로 대체했습니다. 카드 신청 상태에 대한 더 나은 사용자 피드백을 위해 성공 및 실패 아이콘을 추가했습니다. 전체 뷰포트 높이와 중앙 정렬을 위해 레이아웃을 조정했습니다.

--- src/pages/ApplyDone.tsx
+++ src/pages/ApplyDone.tsx
@@ -1,21 +1,77 @@
 import { parse } from 'qs'
-
+import styled from '@emotion/styled'
 import Flex from '@shared/Flex'
 import Text from '@shared/Text'
 import FixedBottomButton from '@shared/FixedBottomButton'
+
+const Container = styled(Flex)`
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+`
+
+const IconWrapper = styled.div`
+    margin-bottom: 24px;
+    svg {
+        width: 120px;
+        height: 120px;
+    }
+`
+
+const ContentWrapper = styled(Flex)`
+    flex: 1;
+    margin-top: -120px; // 상단 여백 조절
+`
+
+function CheckIcon() {
+    return (
+        <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="24" cy="24" r="24" fill="#36B37E" />
+            <path
+                d="M32 18L21 29L16 24"
+                stroke="white"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    )
+}
+
+function CrossIcon() {
+    return (
+        <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="24" cy="24" r="24" fill="#FF5252" />
+            <path
+                d="M30 18L18 30M18 18L30 30"
+                stroke="white"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    )
+}
 
 function ApplyDone() {
     const { success } = parse(window.location.search, {
         ignoreQueryPrefix: true,
     }) as { success: string }
 
+    const isSuccess = success === 'true'
+
     return (
-        <Flex>
-            <Text>
-                {success === 'true'
-                    ? '카드가 발급되었습니다.'
-                    : '카드 발급에 실패했습니다.'}
-            </Text>
+        <Container direction="column">
+            <ContentWrapper direction="column" align="center" justify="center">
+                <IconWrapper>
+                    {isSuccess ? <CheckIcon /> : <CrossIcon />}
+                </IconWrapper>
+                <Text typography="t4" bold>
+                    {isSuccess
+                        ? '카드가 발급되었습니다.'
+                        : '카드 발급에 실패했습니다.'}
+                </Text>
+            </ContentWrapper>
 
             <FixedBottomButton
                 label="확인"
@@ -23,7 +79,7 @@ function ApplyDone() {
                     window.history.back()
                 }}
             />
-        </Flex>
+        </Container>
     )
 }
 
