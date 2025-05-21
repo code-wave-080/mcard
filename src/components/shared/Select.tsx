2f5ede772d4fb9a1db98cb6aaa4842d7508be625
code-wave <codewave080@gmail.com>
2025-05-21 23:22:15 +0900

    feat: Select 컴포넌트에 커스텀 화살표 아이콘 추가
    Select 요소를 새로운 `SelectWrapper`로 감싸고 일관된 스타일링과 사용자 경험 향상을 위해 `StyledChevron`을 추가했습니다. `appearance` 속성을 사용하여 기본 화살표를 제거하고 더 나은 커스터마이징을 구현했습니다.

--- src/components/shared/Select.tsx
+++ src/components/shared/Select.tsx
@@ -13,6 +13,11 @@ interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     placeholder?: string
 }
 
+const SelectWrapper = styled.div`
+    position: relative;
+    width: 100%;
+`
+
 const BaseSelect = styled.select`
     height: 52px;
     background-color: ${colors.grey};
@@ -20,10 +25,41 @@ const BaseSelect = styled.select`
     border-radius: 16px;
     padding: 0 16px;
     cursor: pointer;
+    /* 기본 화살표 제거 */
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
     &:required:invalid {
         color: #c0c4c7;
     }
 `
+
+const StyledChevron = styled(ChevronDown)`
+    position: absolute;
+    right: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 16px;
+    height: 16px;
+    pointer-events: none;
+    color: currentColor;
+`
+
+function ChevronDown() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <polyline points="6 9 12 15 18 9"></polyline>
+        </svg>
+    )
+}
 
 const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select(
     { label, options, placeholder, value, ...props },
@@ -41,16 +77,19 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select(
                     {label}
                 </Text>
             ) : null}
-            <BaseSelect required={true} ref={ref} value={value} {...props}>
-                <option disabled={true} hidden={true} value="">
-                    {placeholder}
-                </option>
-                {options.map(({ label, value }) => (
-                    <option key={label} value={value}>
-                        {label}
+            <SelectWrapper>
+                <BaseSelect required={true} ref={ref} value={value} {...props}>
+                    <option disabled={true} hidden={true} value="">
+                        {placeholder}
                     </option>
-                ))}
-            </BaseSelect>
+                    {options.map(({ label, value }) => (
+                        <option key={label} value={value}>
+                            {label}
+                        </option>
+                    ))}
+                </BaseSelect>
+                <StyledChevron />
+            </SelectWrapper>
         </Flex>
     )
 })
