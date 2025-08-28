1adab56dd04bc900980843b57d1d9923a0faf69e
code-wave <codewave080@gmail.com>
2025-08-28 22:45:14 +0900

    chore: 코드 스타일 정리 및 불필요한 구문 제거

--- src/components/apply/hooks/useAppliedCard.ts
+++ src/components/apply/hooks/useAppliedCard.ts
@@ -3,12 +3,12 @@ import { QueryKey, useQuery, UseQueryOptions } from '@tanstack/react-query'
 import { getAppliedCard } from '@remote/apply'
 import { ApplyValues } from '@models/apply'
 
-type AppliedData = ApplyValues | null;
+type AppliedData = ApplyValues | null
 
 export type AppliedQueryOptions = Omit<
     UseQueryOptions<AppliedData, Error, AppliedData, QueryKey>,
     'onSuccess' | 'onError' | 'onSettled' | 'suspense' | 'queryKey' | 'queryFn'
->;
+>
 
 function useAppliedCard({
     userId,
